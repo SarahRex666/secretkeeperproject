a785902ce9c703070449e41f7c02694ba6ef9096
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-11-18 02:31:51 -0500

    small

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
     include ActionController::Cookies
     before_action :authenticate_user
+    skip_before_action :verify_authenticity_token
   
       private 
   
