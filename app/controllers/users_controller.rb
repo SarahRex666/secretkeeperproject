f8469208f5d7b0afd65d75972a8c4ff3e63c33c9
SarahRex666 <95315055+SarahRex666@users.noreply.github.com>
2022-11-18 10:53:12 -0500

    opened up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-    skip_before_action :authenticate_user, only: [:create, :show]
+    skip_before_action :authenticate_user
     skip_before_action :verify_authenticity_token
 
 
